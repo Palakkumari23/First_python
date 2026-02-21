cfdea11a601389652bedac42a323e312751b19c5
Palakkumari23 <guptapalak872@gmail.com>
2026-02-21 11:31:50 +0530

    Add files via upload

---
+++ factorial of 5.py
@@ -0,0 +1,20 @@
+# namefirstplayer=input("enter name first")
+# namesecoundplayer=input("enter name secound")
+# namethirdplayer=input("enter name third")
+# namefourthplayer=input("enter name fourth")
+# age=int(input("enter your age:"))
+# y=input("do you have ticket(y/n)")
+# if age>=18:
+#     if y=="y":
+#         print("you are eligible to enter in haull")
+# else:
+#     print("you are not eligible to enter in haull")
+# x=int(input("enter a number"))
+# for i in range(6,x,2):
+#  print (i)
+x=int(input("enter a number:"))
+k=1
+for i in range(1,x+1,1):
+    k=k*i
+print(k)
+     
