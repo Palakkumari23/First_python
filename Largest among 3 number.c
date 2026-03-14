faa219f11d9ad5cc127c53f4bb16edf21e8dcffa
Palakkumari23 <guptapalak872@gmail.com>
2026-03-14 19:01:07 +0530

    Add files via upload

---
+++ Largest among 3 number.c
@@ -0,0 +1,9 @@
+a= int(input("enter n:"))
+b =int(input("enter n:"))
+c =int(input("enter n:"))
+if a>b&c:
+print("greatest no.",a)
+elif b>c&a:
+print("greatest no.",b)
+else:
+print("greatest no.",c)
